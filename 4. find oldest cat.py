b58ec7c9f7aea66f8577ee707a9d4096a581689e
Dumpling <zengxinsufe@gmail.com>
2020-12-10 23:10:58 +0800

    Update 4. find oldest cat.py

--- 4. find oldest cat.py
+++ 4. find oldest cat.py
@@ -1,16 +1,17 @@
-# Given the below class:
 class Cat:
     species = 'mammal'
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        
 
 # 1 Instantiate the Cat object with 3 cats
 
 mark = Cat('mark', 2)
 bale = Cat('bale', 3)
 selby = Cat('selby', 1)
+
 
 # 2 Create a function that finds the oldest cat
 
@@ -20,8 +21,10 @@ def find_old_cat(*args):
         if cat.age > oldest_cat.age:
             oldest_cat = cat
     return oldest_cat
-    
+
+
 oldest_cat = find_old_cat(mark, bale, selby)
+
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
 
